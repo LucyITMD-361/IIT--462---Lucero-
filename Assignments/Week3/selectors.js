ab2a57a42105384787ad124b9ff9b4b45e539be8
LucyITMD-361 <Lgarcia11@hawk.iit.edu>
2017-09-12 13:27:40 -0500

    Working on changing element colors from selectors.html

--- Assignments/Week3/selectors.js
+++ Assignments/Week3/selectors.js
@@ -1,1 +1,9 @@
 
+
+$(".a").css("color", "blue");
+$(".b").css("color", "red");
+$(".c").css("color", "pink");
+$(".d").css("color", "yellow");
+$(".e").css("color", "green");
+$(".f").css("color", "orange");
+$(".g").css("color", "brown");
