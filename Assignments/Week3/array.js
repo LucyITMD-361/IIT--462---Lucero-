603b4dd53716d781935584cd82a755526310c7ec
LucyITMD-361 <Lgarcia11@hawk.iit.edu>
2017-09-12 15:36:20 -0500

    Added the solution to finding the highest number in an array

--- Assignments/Week3/array.js
+++ Assignments/Week3/array.js
@@ -1,1 +1,11 @@
-
+function max(){
+  var arrayOfNum = [1, 20, 30, 4, 5, 6, 22, 27, 10, 9];
+  var highestNum = 0;
+  for(var i = 0; i < arrayOfNum.length; i++){
+    if (arrayOfNum[i] > highestNum){
+      highestNum = arrayOfNum[i];
+    }
+  }
+  console.log(highestNum);
+}
+max();
