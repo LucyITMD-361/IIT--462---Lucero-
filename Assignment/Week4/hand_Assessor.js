c0cc2200236bb441a1e6f7217139bf86c3e9d365
LucyITMD-361 <Lgarcia11@hawk.iit.edu>
2017-09-19 17:53:56 -0500

    Update hand_Assessor.js

--- Assignment/Week4/hand_Assessor.js
+++ Assignment/Week4/hand_Assessor.js
@@ -1,1 +1,46 @@
 
+function handAssessor() {
+  var hand = [
+    {"rank": "two", "suit": "spades"}
+    {"rank": "king", "suit": "spades"}
+    {"rank": "two", "suit": "clubs"}
+    {"rank": "four", "suit": "hearts"}
+    {"rank": "eight", "suit": "diamonds"},
+    ];
+}
+
+function containsNTimes(){
+  var containsNTimes;
+  containsNTimes(["two", "king", "two", "four", "eight"], "two", 2);
+  hand.map(function (card) {
+    return card.rank;
+  });
+}
+
+function handRank(){
+  var handRanks,
+  result;
+  handRanks = hand.map(function (card) {
+  return card.rank;
+  });
+  result = containsNTimes(handRanks, "two", 2);
+}
+
+function rank(){
+   var ranks = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace
+    var containsPair = function (hand) {
+        var result = false, handRanks;
+         handRanks = hand.map(function (card) {
+             return card.rank;
+          });
+           ranks.forEach(function (rank) {
+            if (containsNTimes(handRanks, rank, 2)) {
+               result = true;
+               }
+            });
+             return result;
+ }
+ handAssessor();
+             
+             
+             
